746ce5840dd11bbf32b616af3060ba4dc47bd407
Brian <brianbmx_78@hotmail.com>
2021-02-26 20:34:55 -0500

    Primer commit en Bryan

--- js/index.js
+++ js/index.js
@@ -1,42 +1,85 @@
-class PoderCV{
-    constructor(
-        villano,
-        poder
-    ){
-        this.villano = villano;
-        this.poder = poder;
-    }
-
-}
-
-// Referencias al HTML
+// Selectores
 const nuevoPoderBoton = document.querySelector('#nuevoPoderBoton');
-const agregarNuevoPoderBoton = document.querySelector('#agregarNuevoPoderBoton');
+const agregarNuevoPoderBtn = document.querySelector('#agregarNuevoPoderBoton');
 const villanos = document.querySelector('#villanos');
 const superPoder = document.querySelector('#superPoder');
-const poderesArr = [];
+const formulario = document.querySelector('#formulario');
 
-// Eventos
-nuevoPoderBoton.addEventListener('click',(event)=>{
-    const poder = new PoderCV('Daniela','Mete y saca 3000')
-    console.log({poder});
-})
+let editando;
 
-agregarNuevoPoderBoton.addEventListener('click',(event)=>{
-    let poder = superPoder.value;
-    let villano = villanos.value;
-    if(poder && villano){
-        const poderCv = new PoderCV(villano,poder)
-        poderesArr.push(poderCv)
-        localStorage.setItem('poderes',JSON.stringify(poderesArr))
+const poderesObj = {
+    villanos: '',
+    superPoder: '',
+};
+
+listeners();
+function listeners() {
+    formulario.addEventListener('submit', nuevoPoder);
+    villanos.addEventListener('change', datosCV);
+    superPoder.addEventListener('change', datosCV);
+}
+
+class PoderCV{
+    constructor() {
+        this.poder = [];
     }
-})
 
-// Añadir poder en la tabla
-const anadirPoderHtml = (poder)=>{
+    agregarPoder(poderes) {
+        this.poder = [...this.poder, poderes];
+        console.log(this.poder);
+    }
+
 
 }
 
-// superPoder.addEventListener('keyup',(event)=>{
-//     console.log(event.target.value);
-// })
+class UI {
+    mensajeAlertas(mensaje, tipo) {
+        const divMensaje = document.createElement('div');
+        divMensaje.className = 'text-center alert d-block col-12';
+        
+        if( tipo === 'error' ) {
+            divMensaje.classList.remove('alert-success');
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.remove('alert-danger');
+            divMensaje.classList.add('alert-success');
+        }
+
+        divMensaje.innerText = mensaje;
+
+        document.querySelector('.modal-body').insertBefore(divMensaje, formulario);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+const poderCv = new PoderCV();
+const ui = new UI();
+
+function datosCV(e) {
+    poderesObj[e.target.name] = e.target.value;
+}
+
+function nuevoPoder(e) {
+    e.preventDefault();
+    const { villanos, superPoder } = poderesObj
+    if ( superPoder === "" ) {
+        ui.mensajeAlertas('Debes ingresar un super poder','error');
+        return;
+    }
+
+    if( editando ) {
+
+    } else {
+        poderCv.agregarPoder({...poderesObj});
+
+        ui.mensajeAlertas('Se agregó correctamente');
+    }
+
+    formulario.reset();
+
+    
+}
+
