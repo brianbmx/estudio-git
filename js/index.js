cc73725cd099ca57464ca54ba31526876773b7d5
boterosalas <boterosalas@gmail.com>
2021-02-26 20:37:58 -0500

    first commit David

--- js/index.js
+++ js/index.js
@@ -1,41 +1,65 @@
-class PoderCV{
+class PoderCV {
     constructor(
         villano,
         poder
-    ){
+    ) {
         this.villano = villano;
         this.poder = poder;
     }
 
 }
 
+// ``
+
+// Variables
+let poderesArr = [];
+let numeroPoder = 1;
+
 // Referencias al HTML
 const nuevoPoderBoton = document.querySelector('#nuevoPoderBoton');
 const agregarNuevoPoderBoton = document.querySelector('#agregarNuevoPoderBoton');
 const villanos = document.querySelector('#villanos');
 const superPoder = document.querySelector('#superPoder');
-const poderesArr = [];
+const nuevoPoderModal = new bootstrap.Modal(document.querySelector('#nuevoPoderModal'));
+const tbodyPoderes = document.querySelector('#tbodyPoderes');
 
 // Eventos
-nuevoPoderBoton.addEventListener('click',(event)=>{
-    const poder = new PoderCV('Daniela','Mete y saca 3000')
-    console.log({poder});
-})
+// nuevoPoderBoton.addEventListener('click', (event) => {
+// const poder = new PoderCV('Daniela', 'Mete y saca 3000')
+// console.log({ poder });
+// })
 
-agregarNuevoPoderBoton.addEventListener('click',(event)=>{
+agregarNuevoPoderBoton.addEventListener('click', (event) => {
     let poder = superPoder.value;
     let villano = villanos.value;
-    if(poder && villano){
-        const poderCv = new PoderCV(villano,poder)
+    if (poder && villano) {
+        const poderCv = new PoderCV(villano, poder)
         poderesArr.push(poderCv)
-        localStorage.setItem('poderes',JSON.stringify(poderesArr))
+        anadirPoderHtml(poderCv);
+        localStorage.setItem('poderes', JSON.stringify(poderesArr))
+        nuevoPoderModal.hide();
     }
-})
+});
 
-// Añadir poder en la tabla
-const anadirPoderHtml = (poder)=>{
-
+// Funciones
+const anadirPoderHtml = ({ poder, villano }) => {
+    const html = `<th scope="row">${numeroPoder}</th><td>${poder}</td><td>${villano}</td>`;
+    numeroPoder++;
+    const tr = document.createElement('tr');
+    tr.innerHTML = html;
+    tbodyPoderes.append(tr);
 }
+
+const cargarBD = () => {
+    const bd = localStorage.getItem('poderes');
+    if (bd) {
+        poderesArr = JSON.parse(bd);
+        poderesArr.forEach(element => {
+            anadirPoderHtml(element);
+        });
+    }
+}
+cargarBD();
 
 // superPoder.addEventListener('keyup',(event)=>{
 //     console.log(event.target.value);
